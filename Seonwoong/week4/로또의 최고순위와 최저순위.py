79f7a9fe1d09d35f8becc9d1289d7c692f32ed1f
Yoon Seonwoong <neuron4722@naver.com>
2024-09-14 23:20:56 +0900

    Create 로또의 최고순위와 최저순위.py

---
+++ Seonwoong/week4/로또의 최고순위와 최저순위.py
@@ -0,0 +1,10 @@
+def solution(lottos, win_nums):
+
+    rank=[6,6,5,4,3,2,1]
+
+    cnt_0 = lottos.count(0)
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+    return rank[cnt_0 + ans],rank[ans]
